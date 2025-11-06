09299b60f205cb91dd0e286b5012c773e263cd95
olubisiayantoye <olubisiayantoye@gmail.com>
2025-11-06 11:57:28 +0100

    modified:   helloworld.cpp 	modified:   helloworld.exe

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World! Welcome to my first C++ program." << endl;
+    cout << "Hello World! Welcome to my first C++ program. thank you." << endl;
     return 0;
 }
